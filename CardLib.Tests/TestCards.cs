bd4ae94c00adba023abe27c3982d4586db486774
Brian Bollig <Brian.Bollig@granicus.com>
2019-06-18 20:15:17 -0600

    Added TestCards.cs. Added tests for Add().

---
+++ CardLib.Tests/TestCards.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CardLib.Tests
+{
+    [TestClass]
+    public class TestCards
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            Cards cardList = new Cards();
+            Card kingOfSpades = new Card(Suit.Spade, Rank.King);
+
+            Assert.AreEqual(cardList.Count, 0);
+
+            cardList.Add(kingOfSpades);
+
+            Assert.AreNotEqual(cardList.Count, 0);
+            Assert.AreEqual(cardList.Count, 1);
+
+            Assert.IsTrue(cardList[0].Equals(kingOfSpades));
+            Assert.AreEqual<Card>(kingOfSpades, cardList[0]);
+        }
+    }
+}
